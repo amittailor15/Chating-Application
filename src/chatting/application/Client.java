bab3daff5d20372e15c6067f228ee2f8df48b381
Amit Tailor <84769372+amittailor15@users.noreply.github.com>
2022-08-07 00:45:53 +0530

    Update Client.java

--- src/chatting/application/Client.java
+++ src/chatting/application/Client.java
@@ -40,11 +40,11 @@ public class Client implements ActionListener{
         p1.setBounds(0, 0, 450, 70);
         f1.add(p1);
         
-       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3.png"));
+       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
        Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
        ImageIcon i3 = new ImageIcon(i2);
        JLabel l1 = new JLabel(i3);
-       l1.setBounds(5, 17, 30, 30);
+       l1.setBounds(5, 20, 25, 25);
        p1.add(l1);
        
        l1.addMouseListener(new MouseAdapter(){
@@ -53,32 +53,32 @@ public class Client implements ActionListener{
            }
        });
        
-       ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/2.png"));
+       ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/2.png"));
        Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
        ImageIcon i6 = new ImageIcon(i5);
        JLabel l2 = new JLabel(i6);
-       l2.setBounds(40, 5, 60, 60);
+       l2.setBounds(40, 10, 50, 50);
        p1.add(l2);
        
-       ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
+       ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
        ImageIcon i9 = new ImageIcon(i8);
        JLabel l5 = new JLabel(i9);
-       l5.setBounds(290, 20, 30, 30);
+       l5.setBounds(300, 20, 30, 30);
        p1.add(l5);
        
-       ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/phone.png"));
+       ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
        Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
        ImageIcon i13 = new ImageIcon(i12);
        JLabel l6 = new JLabel(i13);
-       l6.setBounds(350, 20, 35, 30);
+       l6.setBounds(360, 20, 35, 30);
        p1.add(l6);
        
-       ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3icon.png"));
+       ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
        Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);
        ImageIcon i16 = new ImageIcon(i15);
        JLabel l7 = new JLabel(i16);
-       l7.setBounds(410, 20, 13, 25);
+       l7.setBounds(420, 20, 10, 25);
        p1.add(l7);
        
        
@@ -92,7 +92,7 @@ public class Client implements ActionListener{
        JLabel l4 = new JLabel("Active Now");
        l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
        l4.setForeground(Color.WHITE);
-       l4.setBounds(110, 35, 100, 20);
+       l4.setBounds(110, 35, 100, 18);
        p1.add(l4);   
        
         Timer t = new Timer(1, new ActionListener(){
@@ -171,7 +171,7 @@ f1.add(sp);
        f1.getContentPane().setBackground(Color.WHITE);
        f1.setLayout(null);
        f1.setSize(450, 700);
-       f1.setLocation(1100, 200); 
+       f1.setLocation(800, 50); 
        f1.setUndecorated(true);
        f1.setVisible(true);
         
